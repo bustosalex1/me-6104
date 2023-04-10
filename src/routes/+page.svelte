48b0992992df4f70d20e23647f261589619a1452
Alex Bustos <bustosalex1@gmail.com>
2023-04-10 04:33:49 -0400

    getting more scaffolding up and running. adding threlte!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex flex-col items-center justify-center min-h-screen min-w-full bg-base-100">
+	<h1 class="text text-3xl font-semibold">ME6104 Final Project: Bézier Curve and Surface Explorer</h1>
+	<p>By Alexander Bustos and Darren Kosen</p>
+</div>
