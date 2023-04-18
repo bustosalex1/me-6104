22d69abfabafad535dab508871c23ed0a9a1e1b1
Alex Bustos <bustosalex1@gmail.com>
2023-04-18 10:26:40 -0400

    Updates

---
+++ _app/immutable/chunks/1.0b4c92af.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.56d4f31f.js";export{t as component};
---
+++ _app/immutable/chunks/2.043a8841.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.61c0bf15.js";export{t as component};
