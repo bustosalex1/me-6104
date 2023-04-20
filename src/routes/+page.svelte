7a6f1de0f2918b2c2c2f6bc7362a6f5410cb3e03
Alex Bustos <bustosalex1@gmail.com>
2023-04-19 22:21:36 -0400

    save me css grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,28 +11,26 @@
 	<title>ME 6104 Final Project</title>
 </svelte:head>
 
-<div class="flex flex-col py-10 w-5/6 mx-auto space-x-2 space-y-6 justify-center">
-	<div class="text-center flex flex-col justify-end bg-base-100">
+<div class="grid grid-rows-auto py-20 grid-cols-1 lg:grid-cols-6 px-20 min-h-screen w-full gap-2">
+	<div
+		class="text-center flex flex-col justify-end bg-base-100 col-start-1 col-span-full row-start-1 row-span-1"
+	>
 		<h1 class="text text-3xl font-semibold">
 			ME6104 Final Project: Bézier Curve and Surface Explorer
 		</h1>
 		<p>By Alexander Bustos and Darren Kosen</p>
 	</div>
-	<div class="flex flex-col space-y-2 justify-center lg:flex-row lg:space-x-2 lg:space-y-0 h-full">
-		<div
-			class="border border-black rounded-md drop-shadow-md bg-base-100 w-full lg:w-3/4 aspect-video viewer"
-		>
+	<div class="row-start-2 row-span-1 lg:row-span-3 col-start-1 col-span-full lg:col-span-4">
+		<div class="border border-black rounded-md drop-shadow-md bg-base-100 w-full h-full">
 			<Viewer bind:curve bind:surface />
 		</div>
+	</div>
 
-		<div class="border border-black rounded-md drop-shadow-md bg-base-100 w-full lg:w-1/4">
+	<div
+		class="row-start-3 lg:row-start-2 row-span-1 lg:row-span-3 col-start-1 lg:col-start-5 col-span-1 lg:col-span-2"
+	>
+		<div class="border border-black rounded-md drop-shadow-md bg-base-100 w-full h-full">
 			<ControlPanel bind:curve bind:surface />
 		</div>
 	</div>
 </div>
-
-<style>
-	.viewer {
-		height: 750px;
-	}
-</style>
